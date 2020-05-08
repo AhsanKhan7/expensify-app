166f6cd6e5e6fd91f66e14df9a24146a65874e06
unknown <innocent.ahsan12345@gmail.com>
2020-05-08 01:08:03 -0700

    Resolved some firebase issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,31 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_key,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-  };  
+    apiKey: "AIzaSyAh-v55BHz62svl_3YHWUuwASknYHvaBNk",
+    authDomain: "expensify-12366.firebaseapp.com",
+    databaseURL: "https://expensify-12366.firebaseio.com",
+    projectId: "expensify-12366",
+    storageBucket: "expensify-12366.appspot.com",
+    messagingSenderId: "395660110020",
+    appId: "1:395660110020:web:f02ef95f127c3ecd898191"
+}
+
 firebase.initializeApp(firebaseConfig);
 
-
 const database = firebase.database()
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export { firebase, googleAuthProvider, database as default }
+
+// const firebaseConfig = {
+//     apiKey: process.env.FIREBASE_API_key,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.FIREBASE_DATABASE_URL,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID
+//   };
 
 //child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
